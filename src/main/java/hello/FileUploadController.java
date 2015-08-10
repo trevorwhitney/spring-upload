d222c40cfd739f279e49a5b634ef29d7c06043f9
Trevor Whitney <twhitney@pivotal.io>
2015-08-10 16:13:06 -0600

    finished spring upload file tutorioal

---
+++ src/main/java/hello/FileUploadController.java
@@ -0,0 +1,36 @@
+package hello;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUploadController {
+
+    @RequestMapping(value="/upload", method=RequestMethod.GET)
+    public @ResponseBody String provideUploadInfo() {
+        return "You can upload a file by posting to this same URL.";
+    }
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
+                                                 @RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name)));
+                stream.write(bytes);
+                stream.close();
+                return String.format("You successfully uploaded %s !", name);
+            } catch (Exception e) {
+                return String.format("You failed to upload %s because %s.", name, e.getMessage());
+            }
+        } else {
+            return String.format("You failed to upload %s because the file was empty.", name);
+        }
+    }
+}
